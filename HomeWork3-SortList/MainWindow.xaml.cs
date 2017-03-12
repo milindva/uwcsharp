b7e5bc751774d37607d44dc00547a9f819819b74
Milind Vakil <milind.vakil@gmail.com>
2017-03-12 01:00:33 -0800

    Added homework 3

---
+++ HomeWork3-SortList/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool uxNameHasText = false;
+        bool uxPasswordHasText = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Setting the cursor/focus on Name text box for user entry
+
+            uxName.Focus();
+
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password:" + uxPassword.Password);
+
+            //Clear contents of both the text boxes after submit button is clicked
+            // and set the focus back to Name text box
+            //Disable Submit button after it has been clicked 
+
+
+            uxName.Clear();
+            uxPassword.Clear();
+            uxSubmit.IsEnabled = false;
+
+            uxName.Focus();
+            uxNameHasText = false;
+            uxPasswordHasText = false;
+
+            var window = new SecondWindow();
+            Application.Current.MainWindow = window;
+            Close();
+            window.Show();
+
+
+        }
+
+        // method for validating and enabling Submit button 
+        private void ValidateSubmitEnabled()
+        {
+            if (uxNameHasText == true && uxPasswordHasText == true)
+                uxSubmit.IsEnabled = true;
+            else
+                uxSubmit.IsEnabled = false;
+
+        }
+
+        private void uxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+            // check if there is at least one character entered in Name text box
+            if (uxName.Text.Length > 0)
+                uxNameHasText = true;
+            else
+                uxNameHasText = false;
+
+            ValidateSubmitEnabled();
+        }
+
+
+
+        private void uxPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            // check if there is at least one character entered in Password text box
+
+            if (uxPassword.Password.Length > 0)
+                uxPasswordHasText = true;
+            else
+                uxPasswordHasText = false;
+
+            ValidateSubmitEnabled();
+
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // MessageBox.Show("Checbox is checked");
+        }
+
+        public override void EndInit()
+        {
+            base.EndInit();
+
+         
+        }
+
+
+    }
+}
+
---
+++ HomeWork3-SortList/SecondWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+
+        private GridViewColumnHeader listViewSortCol = null;
+        private ListSortDirection dirToSort = ListSortDirection.Ascending;
+
+        public SecondWindow()
+        {
+
+
+            InitializeComponent();
+
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "1DavePwd" });
+            users.Add(new Models.User { Name = "Steve", Password = "2StevePwd" });
+            users.Add(new Models.User { Name = "Lisa", Password = "3LisaPwd" });
+            // users.Add(new Models.User { Name = "Test", Password = "3TestPwd" });
+
+            uxList.ItemsSource = users;
+
+
+
+        }
+
+        private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            GridViewColumnHeader column = (sender as GridViewColumnHeader);
+
+            string sortBy = column.Tag.ToString();
+
+            // if the  column is clicked for the first time , sort in ascending direction
+            if (listViewSortCol != null) 
+                uxList.Items.SortDescriptions.Clear();
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+
+            if (listViewSortCol == column) //if the same column is clicked again, change sort direction
+                newDir = ListSortDirection.Descending;
+
+            uxList.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+
+            listViewSortCol = column;
+
+
+        }
+
+
+    }
+
+
+}
+
